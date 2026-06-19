c5c7a066aed0d078b688995c1aae4d8fe5cd00c1
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unipdu.ac.id>
2026-06-19 19:29:48 +0300

    test controller

--- app/Http/Controllers/MobilController.php
+++ app/Http/Controllers/MobilController.php
@@ -8,10 +8,9 @@ use Illuminate\Http\Request;
 class MobilController extends Controller
 {
     public function index()
-    {
-        $mobils = Mobil::all();
-        return view('mobil.index', compact('mobils'));
-    }
+{
+    return 'Controller OK';
+}
 
     public function create()
     {
