7ceac810ddb007cf7ebd54dcf0ae193cddfbcb1f
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unipdu.ac.id>
2026-06-20 13:53:28 +0300

    add session test route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MobilController;
 
 Route::get('/', function () {
     return redirect('/mobil');
+});
+
+Route::get('/test-session', function () {
+    session(['cek' => 'halo']);
+    return response()->json([
+        'session_id' => session()->getId(),
+        'session_driver' => config('session.driver'),
+        'has_value' => session('cek'),
+        'all_session' => session()->all(),
+    ]);
 });
 
 Route::resource('mobil', MobilController::class);
