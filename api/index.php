73c77711142cc61dd70d95202d6df06a002c4990
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unipdu.ac.id>
2026-06-20 12:11:51 +0300

    debug step by step laravel boot

--- api/index.php
+++ api/index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Test berhasil, PHP jalan!";
+
+echo "Step 1: PHP OK<br>";
+
+require __DIR__ . '/../vendor/autoload.php';
+echo "Step 2: Autoload OK<br>";
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+echo "Step 3: App bootstrap OK<br>";
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+echo "Step 4: Kernel OK<br>";
