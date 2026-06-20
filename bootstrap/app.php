a2fd36198f626afdadbe8c06515d0220e34ded82
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unipdu.ac.id>
2026-06-20 14:33:35 +0300

    kembalikan middleware awal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,21 +1,18 @@
-    <?php
+<?php
 
-    use Illuminate\Foundation\Application;
-    use Illuminate\Foundation\Configuration\Exceptions;
-    use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-    return Application::configure(basePath: dirname(__DIR__))
-        ->withRouting(
-            web: __DIR__.'/../routes/web.php',
-            commands: __DIR__.'/../routes/console.php',
-            health: '/up',
-        )
-        ->withMiddleware(function ($middleware) {
-    $middleware->validateCsrfTokens(except: [
-        'mobil',
-        'mobil/*',
-    ]);
-})
-        ->withExceptions(function (Exceptions $exceptions): void {
-            //
-        })->create();
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function ($middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
