bbecdb2ba0f77634e523b2086c61fa7d5dfb7ab4
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unipdu.ac.id>
2026-06-20 12:10:17 +0300

    test minimal php output

--- api/index.php
+++ api/index.php
@@ -1,7 +1,2 @@
 <?php
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-require __DIR__ . '/../public/index.php';
+echo "Test berhasil, PHP jalan!";
